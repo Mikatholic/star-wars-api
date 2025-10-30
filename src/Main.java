467d02ecab13e324c5ad22e396f9cbb14e90b22a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-30 16:51:46 -0500

    Agregando mensaje

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
         ConsultaPelicula consulta = new ConsultaPelicula();
+        System.out.println("Escriba el número de la película que quiere consultar");
         try{
             Pelicula pelicula = consulta.buscaPelicula(9);
             System.out.println(pelicula);
