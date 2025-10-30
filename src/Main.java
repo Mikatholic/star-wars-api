5ab89494db5391eaeecee0863d22784b88ea1102
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-30 13:48:45 -0500

    Agregando un System.out.println para probar el còdigo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         ConsultaPelicula consulta = new ConsultaPelicula();
         Pelicula pelicula = consulta.buscaPelicula(1);
+        System.out.println(pelicula);
     }
 }
