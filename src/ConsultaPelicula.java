54e18a54613a00f0aa2c08d4334a82efb8eebf00
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-30 13:09:09 -0500

    Importando HttpResponse

--- src/ConsultaPelicula.java
+++ src/ConsultaPelicula.java
@@ -1,6 +1,7 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultaPelicula {
     URI direccion = URI.create("https://swapi.dev/api/films/" + numeroDePelicula);
