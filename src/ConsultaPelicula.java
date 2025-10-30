f3121cde08d20c08569c1a6f1f0a714a9a186f4a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-30 13:30:50 -0500

    Corrigiendo el código

--- src/ConsultaPelicula.java
+++ src/ConsultaPelicula.java
@@ -1,22 +1,31 @@
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaPelicula {
-    URI direccion = URI.create("https://swapi.dev/api/films/" + numeroDePelicula);
 
     Pelicula buscaPelicula(int numeroDePelicula){
+        URI direccion = URI.create("https://swapi.dev/api/films/" + numeroDePelicula);
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri()
+                .uri(direccion)
                 .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpRequest.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
         return new Gson().fromJson(response.body(), Pelicula.class);
     }
