83fdc2f2122d455506fb8343f8b4425623259020
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-30 12:56:38 -0500

    Agregando HttpClient y HttpRequest

--- src/ConsultaPelicula.java
+++ src/ConsultaPelicula.java
@@ -1,6 +1,10 @@
 public class ConsultaPelicula {
 
     Pelicula buscaPelicula(int numeroDePelicula){
+        HttpClient client = HttpClient.newHttpClient();
 
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://foo.com/"))
+                .build();
     }
 }
